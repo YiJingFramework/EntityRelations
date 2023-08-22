6e129f41d08334b667748e12bf9186a8de0fc59b
yueyinqiu <lujiaqi2003@126.com>
2023-08-22 11:22:34 +0800

    240

---
+++ src/EntityRelations/TianganJigongs/Extensions/TianganJigongExtensions.cs
@@ -0,0 +1,37 @@
+﻿using YiJingFramework.PrimitiveTypes;
+
+namespace YiJingFramework.EntityRelations.TianganJigongs.Extensions;
+
+/// <summary>
+/// 关于天干寄宫的扩展。
+/// Extensions about Tiangans' Jigong.
+/// </summary>
+public static class TianganJigongExtensions
+{
+    /// <summary>
+    /// 获取天干的寄宫。
+    /// Get the Tiangan's Jigong.
+    /// </summary>
+    /// <param name="tiangan">
+    /// 天干。
+    /// The Tiangan.
+    /// </param>
+    /// <returns>
+    /// 结果。
+    /// The result.
+    /// </returns>
+    public static Dizhi Jigong(this Tiangan tiangan)
+    {
+        return new(tiangan.Index switch
+        {
+            1 => 3,
+            2 => 5,
+            3 or 5 => 6,
+            4 or 6 => 8,
+            7 => 9,
+            8 => 11,
+            9 => 12,
+            _ => 2
+        });
+    }
+}
---
+++ src/EntityRelationsTests/TianganJigongs/Extensions/TianganJigongExtensionsTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YiJingFramework.EntityRelations.TianganJigongs.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YiJingFramework.PrimitiveTypes;
+using YiJingFramework.EntityRelations.ShierZhangshengs.Extensions;
+using YiJingFramework.EntityRelations.EntityCharacteristics.Extensions;
+using YiJingFramework.EntityRelations.ShierZhangshengs;
+
+namespace YiJingFramework.EntityRelations.TianganJigongs.Extensions.Tests;
+
+[TestClass()]
+public class TianganJigongExtensionsTests
+{
+    [TestMethod()]
+    public void JigongTest()
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            var tiangan = new Tiangan(i);
+            if (tiangan.Yinyang().IsYang)
+            {
+                Assert.AreEqual(tiangan.ShierZhangsheng(ShierZhangsheng.Linguan), tiangan.Jigong());
+            }
+            else
+            {
+                Assert.AreEqual(tiangan.ShierZhangsheng(ShierZhangsheng.Guandai), tiangan.Jigong());
+            }
+        }
+    }
+}
